10477405bea76c7236c3fbbb3564ca4b3ea8597f
Chris Chapman <chrischapman900@gmail.com>
2013-05-19 22:38:38 -0400

    Initial Commit

---
+++ Matchismo/CardGameAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CardGameAppDelegate.h
+//  Matchismo
+//
+//  Created by Chris on 5/19/13.
+//  Copyright (c) 2013 Chris Chapman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Matchismo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Matchismo
+//
+//  Created by Chris on 5/19/13.
+//  Copyright (c) 2013 Chris Chapman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CardGameAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CardGameAppDelegate class]));
+    }
+}
